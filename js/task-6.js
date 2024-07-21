cfafacee37a28cd02686220ea3719fd3c9f64bd7
Ziborov Serhii <144032720+Serhii01010101@users.noreply.github.com>
2024-07-21 17:44:08 +0300

    v1.3

--- js/task-6.js
+++ js/task-6.js
@@ -8,13 +8,14 @@ createBtn.addEventListener("click", onCreateBtnClick);
 destroyBtn.addEventListener("click", onDestroyBtnClick);
 
 function onCreateBtnClick() {
-  if (inputEl.value >= 1 && inputEl.value <= 100) {
+  const inputValue = parseInt(inputEl.value, 10);
+  if (inputValue >= 1 && inputValue <= 100) {
     destroyBoxes();
-    createBoxes(inputEl.value);
+    createBoxes(inputValue);
     inputEl.value = "";
     box.classList.add("box-grid");
   } else {
-    alert(`Enter number between 1 and 100`);
+    alert("Enter number between 1 and 100");
   }
 }
 
@@ -25,21 +26,25 @@ function onDestroyBtnClick() {
 function createBoxes(amount) {
   let width = 30;
   let height = 30;
+  const fragment = document.createDocumentFragment();
 
   for (let i = 0; i < amount; i += 1) {
     const createdDiv = document.createElement("div");
     createdDiv.style.width = width + "px";
     createdDiv.style.height = height + "px";
     createdDiv.style.backgroundColor = getRandomHexColor();
-    box.append(createdDiv);
+    fragment.appendChild(createdDiv);
     width += 10;
     height += 10;
   }
+
+  box.appendChild(fragment);
 }
 
 function destroyBoxes() {
   box.innerHTML = "";
 }
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
